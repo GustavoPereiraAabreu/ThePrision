4bf077ce368df2e699da99b522f7119acc01bf1e
GUSTAVO PEREIRA ABREU <gustavop.4breu@gmail.com>
2026-03-09 10:48:04 -0300

    BossLife

--- Assets/Gustavo/Scritps-GU/BossLife.cs
+++ Assets/Gustavo/Scritps-GU/BossLife.cs
@@ -1,13 +1,37 @@
 using UnityEngine;
+using System.Collections; 
 
 public class BossLife : MonoBehaviour
 {
     public int life = 3;
     public GameObject victoryScreen;
 
+    [Header("Configurações de Visual")]
+    public Color damageColor = Color.red;
+    public float flashDuration = 0.1f;
+
+    private SpriteRenderer spriteRenderer;
+    private Color originalColor;
+
+    void Start()
+    {
+        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            originalColor = spriteRenderer.color;
+        }
+    }
+
     public void TakeDamage()
     {
         life--;
+
+       
+        if (spriteRenderer != null)
+        {
+            StartCoroutine(FlashDamage());
+        }
 
         if (life <= 0)
         {
@@ -15,9 +39,16 @@ public class BossLife : MonoBehaviour
         }
     }
 
+   
+    IEnumerator FlashDamage()
+    {
+        spriteRenderer.color = damageColor; 
+        yield return new WaitForSeconds(flashDuration); 
+        spriteRenderer.color = originalColor; 
+    }
+
     void Die()
     {
-
         if (victoryScreen != null)
         {
             victoryScreen.SetActive(true);
